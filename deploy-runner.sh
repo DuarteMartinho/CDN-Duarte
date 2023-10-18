dcf34aeb1d2edde44f09d245cf0c332adbe23795
DuarteMartinho <duartemartinho11@gmail.com>
2023-10-18 17:07:25 +0100

    fix: Edit deploy files

---
+++ deploy-runner.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Function to copy contents on shutdown
+copy_contents() {
+  echo "Copying contents from /srv/app/public/images to ./public/images"
+  cp -r /srv/app/public/images/* ./public/images/
+  echo "Contents copied successfully"
+}
+
+# Trap the SIGTERM signal and call the copy_contents function
+trap 'copy_contents' SIGTERM
+
+# Start your application
+npm i
+npm run start
